d8df4340395935577e016b4f887c5994f5df6e9c
Owen Scott <owen.m.scott@gmail.com>
2013-08-09 16:51:20 -0400

    new schema and global

---
+++ global/globals.js
@@ -0,0 +1,16 @@
+getTemplatePaths = function () {
+    return ['location-geocoding.csv'];
+}
+
+getDbName = function() {
+    return 'oapNepal';
+}
+
+getLogFileName = function() {
+    return 'logFile.log';
+}
+
+getSchemaPaths = function() {
+    return ['location','project'];
+}
+
